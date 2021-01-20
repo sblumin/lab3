fd706719542505a125be5033017d232bae359108
Sara Blumin <sblumin@ucsd.edu>
2021-01-20 12:24:06 -0800

    Added to js file for click listeners and animations

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,22 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Testing...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
 }
